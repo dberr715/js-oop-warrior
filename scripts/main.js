c5873c0fd9778e7a91448a16850357886af4740a
David Berry <dberry3755@gmail.com>
2023-09-22 22:39:44 -0400

    more buttons, health bars update, but no message shows

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,26 @@
 "use strict";
+let heroName;
+let villainName;
+let MrFantastic;
+let DrEvil;
+let heroInput;
+let villainInput;
+let twoFighterButton;
+let heroAttackVilButton;
+let heroAttackZombButton;
+let villAttackHeroButton;
+let randomEnemyAttackHeroButton;
+let heroHealthBar;
+let villainHealthBar;
+
+const fee = document.querySelector("#fee");
+const fi = document.querySelector("#fi");
+const fo = document.querySelector("#fo");
+const fum = document.querySelector("#fum");
+const greeting1 = document.querySelector("#greeting1");
+const greeting2 = document.querySelector("#greeting2");
+const announce = document.querySelector("#announce");
+const taunt = document.querySelector("#taunt");
 
 class Warrior {
   constructor(characterName, details, health = 10, power = 5) {
@@ -8,15 +30,18 @@ class Warrior {
     this.details = details;
   }
 
-  greet(other_character) {
-    console.log(
-      `Hey ho ${other_character.characterName}! Tis I, ${this.characterName}`
-    );
+  greet(div) {
+    const greetMessage = `You chose ${this.characterName} for battle 💪🏼`;
+    console.log(div);
+    div.textContent = greetMessage;
   }
 
   attack(other_character) {
     const health = other_character.health - this.power;
     other_character.health = Math.max(health, 0);
+  }
+  updateH(div) {
+    div.textContent = `Current Health: ${this.health}`;
   }
 
   alive() {
@@ -36,9 +61,9 @@ class Hero extends Warrior {
     super(characterName, details, health, power);
   }
   announce(villain) {
-    console.log(
-      `Prepare to be defeated, ${villain.characterName} by the mighty hand of ${this.characterName}`
-    );
+    const announceMessage = `Prepare to be defeated, ${villain.characterName} by the mighty hand of ${this.characterName}`;
+    const announceElement = document.getElementById("announce");
+    announceElement.textContent = announceMessage;
   }
 }
 
@@ -47,9 +72,9 @@ class Villain extends Warrior {
     super(characterName, details, health, power);
   }
   taunt(hero) {
-    console.log(
-      `Too bad you are outnumbered, ${hero.characterName}.  I have 10 zombies at my disposal. It is time to die!! `
-    );
+    const tauntMessage = `Too bad you are outnumbered, ${hero.characterName}.  I have 10 zombies at my disposal. It is time to die!!`;
+    const tauntElement = document.getElementById("taunt");
+    tauntElement.textContent = tauntMessage;
   }
 }
 class Zombie extends Villain {
@@ -58,9 +83,8 @@ class Zombie extends Villain {
   }
 }
 
-const DrEvil = new Villain("DrEvil", "likes the color green", 15, 10);
-
-const MrFantastic = new Hero("MrFantastic", "likes the color red", 25, 5);
+// const DrEvil = new Villain("DrEvil", "likes the color green", 15, 10);
+// console.log({ heroInput });
 
 const Zombie1 = new Zombie("Zombie1", "likes brains", 5, 3);
 const Zombie2 = new Zombie("Zombie2", "likes brains", 5, 3);
@@ -92,7 +116,7 @@ enemies.push(
 const heroNextMove = (hero) => {
   if (hero.alive() === true) {
     hero.attack(DrEvil);
-    console.log(`DrEvil Health: ${DrEvil.health}`);
+    console.log(`VILLAIN Health: ${DrEvil.health}`);
   } else {
     console.log(`Alas, ${hero.characterName} is dead!`);
   }
@@ -109,85 +133,198 @@ const randomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-//...........................................................................................
-//...........................................................................................
-//...........................................................................................
+//..........................
+//..........................
+//..........................
+//..........................
 // DOM creation stuff (buttons, inputs....)
-//...........................................................................................
-//...........................................................................................
-//initial choose fighter button
-document.addEventListener("DOMContentLoaded", () => {
+
+const startGame = () => {
+  console.log("start game");
+  removePrevious("villainInputButton");
+  removePrevious("villainInput");
+};
+
+const removePrevious = (id) => {
+  console.log("remove");
+  const previousThing = document.getElementById(id);
+  if (previousThing) {
+    previousThing.remove();
+  }
+};
+
+const createBeginButton = () => {
+  console.log("create begin");
   const beginButton = document.createElement("button");
+
   beginButton.id = "beginButton";
   beginButton.textContent = "Choose your fighters!";
-  const fee = document.querySelector("#fee");
   fee.appendChild(beginButton);
+  console.log(beginButton);
+};
+createBeginButton();
 
-  //WILL REUSE ***remove previous button on screen function
-  const removePrevious = (id) => {
-    const previousThing = document.getElementById(id);
-    if (previousThing) {
-      previousThing.remove();
+const createHeroInput = () => {
+  removePrevious("beginButton");
+  const heroInputButton = document.createElement("button");
+  console.log(heroInputButton);
+  heroInputButton.id = "heroInputButton";
+  heroInputButton.textContent = "Confirm your HERO!";
+  heroInput = document.createElement("input");
+  heroInput.type = "text";
+  heroInput.id = "heroInput";
+  heroInput.placeholder = "Enter the name of your HERO!";
+  fee.appendChild(heroInput);
+  fi.appendChild(heroInputButton);
+
+  heroInputButton.addEventListener("click", () => {
+    createVillainInput();
+  });
+};
+
+const createVillainInput = () => {
+  heroName = document.getElementById("heroInput").value;
+  MrFantastic = new Hero(heroName, "likes the color red", 25, 5);
+  MrFantastic.greet(dialogue2);
+
+  const villainInputButton = document.createElement("button");
+  villainInputButton.id = "villainInputButton";
+  villainInputButton.textContent = "Confirm your VILLAIN!";
+  villainInputButton.addEventListener("click", startGame);
+
+  villainInput = document.createElement("input");
+  console.log({ villainInput });
+  villainInput.type = "text";
+  villainInput.id = "villainInput";
+  villainInput.placeholder = "Enter the name of your VILLAIN!";
+  fee.appendChild(villainInput);
+  fi.appendChild(villainInputButton);
+
+  console.log("OUT", villainInput.value);
+  villainInputButton.addEventListener("click", () => {
+    console.log("IN", villainInput.value);
+    getVillainValue();
+    twoFightersStart();
+    // loadBattleButtons();
+  });
+
+  removePrevious("heroInputButton");
+  removePrevious("heroInput");
+};
+const getVillainValue = () => {
+  console.log({ villainName });
+  villainName = villainInput.value;
+  DrEvil = new Villain(villainName, "likes the color green", 15, 10);
+  DrEvil.greet(dialogue3);
+};
+const twoFightersStart = () => {
+  twoFighterButton = document.createElement("button");
+  twoFighterButton.id = "twoFighterButton";
+  twoFighterButton.textContent = "LET THE FIGHTING COMMENCE!";
+  fum.appendChild(twoFighterButton);
+
+  twoFighterButton.addEventListener("click", () => {
+    removePrevious("twoFighterButton");
+
+    setTimeout(() => {
+      MrFantastic.announce(DrEvil);
+      setTimeout(() => {
+        DrEvil.taunt(MrFantastic);
+        setTimeout(() => {
+          loadBattleButtons();
+        }, 2000);
+      }, 1500);
+    }, 500);
+  });
+};
+
+const loadBattleButtons = () => {
+  console.log("Battle buttons loading");
+  heroAttackVilButton = document.createElement("button");
+  heroAttackZombButton = document.createElement("button");
+  randomEnemyAttackHeroButton = document.createElement("button");
+
+  heroAttackVilButton.addEventListener("click", () => {
+    MrFantastic.attack(DrEvil);
+    const villainHealthDiv = document.getElementById("villainHealth");
+    villainHealthDiv.textContent = `Villain Health: ${DrEvil.health} points`;
+    const villainHealthBar = document.getElementById("villainHealthBar");
+    villainHealthBar.value = DrEvil.health;
+    removePrevious("dialogue2");
+    removePrevious("dialogue3");
+    const dialogue = document.getElementById("dialogue2");
+    dialogue.textContent = `${MrFantastic.characterName} attacks ${DrEvil.characterName} for ${MrFantastic.power} damage!`;
+    if (DrEvil.health <= 0) {
+      dialogue.textContent += ` ${DrEvil.characterName} has been defeated!`;
     }
-  };
 
-  //input for hero name
-  beginButton.addEventListener("click", () => {
-    removePrevious("beginButton");
-    const heroInput = document.createElement("input");
-    heroInput.type = "text";
-    heroInput.id = "heroInput";
-    fee.appendChild(heroInput);
-    heroInput.placeholder = "Enter the name of your HERO!";
-    const heroInputButton = document.createElement("button");
-    heroInputButton.id = "heroInputButton";
-    heroInputButton.textContent = "Confirm your HERO!";
-    const fi = document.querySelector("#fi");
-    fi.appendChild(heroInputButton);
+    const heroAttackZombieButton = document.getElementById(
+      "heroAttackZombButton"
+    );
+
+    heroAttackZombieButton.addEventListener("click", () => {
+      const dialogue2 = document.getElementById("dialogue2");
+      dialogue2.textContent = ""; // Clear existing content
+
+      const message = `${MrFantastic.characterName} attacks ${DrEvil.characterName}'s zombie minions for ${MrFantastic.power} damage! But they are undead, SO THEY CAN NEVER DIE! Useless attack!`;
+      dialogue2.textContent = message;
+    });
+  });
+  heroHealthBar = document.createElement("progress");
+  villainHealthBar = document.createElement("progress");
+
+  heroAttackVilButton.id = "heroAttackVilButton";
+
+  randomEnemyAttackHeroButton.id = "randomEnemyAttackHeroButton";
+
+  heroHealthBar.id = "heroHealthBar";
+  villainHealthBar.id = "villainHealthBar";
+
+  heroAttackVilButton.textContent = "Hero Attack on Villain";
+
+  randomEnemyAttackHeroButton.textContent = "Villain Team Attack on Hero";
+
+  heroAttackZombButton = document.createElement("button");
+  heroAttackZombButton.id = "heroAttackZombButton";
+  heroAttackZombButton.textContent = "Hero Attack on Zombies";
+
+  heroAttackZombButton.addEventListener("click", () => {
+    const dialogue2 = document.getElementById("dialogue2");
+    dialogue2.textContent = "";
+    dialogue3.textContent = "";
+    dialogue4.textContent = "";
+    dialogue5.textContent = "";
+    greeting1.textContent = "";
+    greeting2.textContent = "";
+    taunt.textContent = "";
+    announce.textContent = ""; // Clear existing content
+
+    const message = `${MrFantastic.characterName} attacks ${DrEvil.characterName}'s zombie minions  But they are undead, SO ATTACK IS USELESS`;
+    dialogue2.textContent = message;
   });
 
-  //confirm your HERO button to start greeting
-  heroInputButton.addEventListener("click", () => {
-    removePrevious("heroInputButton");
-    removePrevious("heroInput");
-    const heroGreeting = document.createElement("p");
-    heroGreeting.id = "heroGreeting";
-    const fi = document.querySelector("#fi");
-    fi.appendChild(heroGreeting);
+  fee.appendChild(heroAttackZombButton);
+
+  fee.appendChild(heroAttackVilButton);
+  fee.appendChild(heroAttackZombButton);
+  fi.appendChild(randomEnemyAttackHeroButton);
+
+  randomEnemyAttackHeroButton.addEventListener("click", () => {
+    villainNextMove();
+    const heroHealthDiv = document.getElementById("heroHealth");
+
+// Update the hero health text content
+heroHealthDiv.textContent = `Hero Health: ${MrFantastic.health} points`;
+    const heroHealthBar = document.getElementById("heroHealthBar");
+    heroHealthBar.value = MrFantastic.health;
+    removePrevious("dialogue2");
+    removePrevious("dialogue3");
+    const dialogue = document.getElementById("dialogue2");
+    dialogue.textContent = `${DrEvil.characterName}'s team attacks ${MrFantastic.characterName} for ${DrEvil.power} damage!`;
+    if (MrFantastic.health <= 0) {
+      dialogue.textContent += ` ${MrFantastic.characterName} has been defeated!`;
+    }
   });
+};
 
-  //need to do previous steps first
-  //input for villain name
-  heroInputButton.addEventListener("click", () => {
-    removePrevious("heroInput");
-    const villainInput = document.createElement("input");
-    villainInput.type = "text";
-    villainInput.id = "heroInput";
-    fee.appendChild(villainInputInput);
-    villainInputInput.placeholder = "Enter the name of your VILLAIN!";
-    const villainInputButton = document.createElement("button");
-    villainInputButton.id = "heroInputButton";
-    villainInputButton.textContent = "Confirm your VILLAIN!";
-    const fi = document.querySelector("#fi");
-    fi.appendChild(villainInputButton);
-  });
-
-  //confirm your VILLAIN button to start greeting
-
-  //Hero announcement
-
-  //Villain taunt
-
-  //Button to start battle
-
-  //Present options of (hero attack zombie, hero attack main villain, villain attack hero,
-  //random attack on hero by any villain (including zombies)
-
-  //log health point updates in DOM
-
-  //repeat previous 2 steps
-
-  //Once victory is won// victory cry for hero or villain, fun stuff on screen
-
-  //button to make new battle
-});
+beginButton.addEventListener("click", createHeroInput);
